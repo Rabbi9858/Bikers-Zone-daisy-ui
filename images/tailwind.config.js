1ccb17d4d4529388ad20b20354c0cb69b9b4598d
Rabbi Hossain <rabbihossainblog@gmail.com>
2024-05-07 20:50:55 +0600

    Biker zone web completed

---
+++ images/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { "light"},
+  },
+  plugins: [required{daisyui}],
+}
+
+
